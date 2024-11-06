eabb50370935fb6229eb78bedaa73e069b425323
Jose Miguel Loyola <coteloyola@hotmail.com>
2024-11-06 03:09:12 -0300

    fix modified documents

--- controller/finance/commonMovements/readModifiedDocuments.php
+++ controller/finance/commonMovements/readModifiedDocuments.php
@@ -1,5 +1,5 @@
 <?php
-require_once './fileManager.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/controller/finance/commonMovements/fileManager.php';
 
 $sessionManager = new sessionManager();
 $fileManager = new FileManager(__DIR__.'/modifiedMovementsFiles');
